75c6e5071cbb4ccabc7e44f88dca6602bb47f05e
GKDevotion <gk@devotiontech.io>
2026-04-21 12:03:33 +0530

    update header style

--- elements/footer.php
+++ elements/footer.php
@@ -138,6 +138,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/custom.js"></script>
         <script>
             if( $('#serviceTrack').length >0 ){
                 const track = document.getElementById('serviceTrack');
